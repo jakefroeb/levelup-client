9f959bc7664732bb100fe673ca500c5857148ba8
jakefroeb <jake.herman.froeb@gmail.com>
2021-05-12 10:45:04 -0500

    finished supplemental chapters

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -12,14 +12,18 @@ export const GameList = (props) => {
 
     return (
         <article className="games">
+            
             <button className="btn btn-2 btn-sep icon-create" onClick={() => {
                 history.push({ pathname: "/games/new" })}}
             >Register New Game</button>
             {
                 games.map(game => {
+                    console.log(game)
                     return <section key={`game--${game.id}`} className="game">
                         <div className="game__title"><h2>{game.name}</h2></div>
                         <div className="game__type">type : {game.game_type.name}</div>
+                        <div className="game__event_count">number of events: {game.event_count}</div>
+                        <div className="game__event_count">number of events I am hosting: {game.user_event_count}</div>
                         <button className="editButton" onClick = {() => {
                             history.push({ pathname: `/games/edit/${game.id}`})
                         }}>edit</button>
